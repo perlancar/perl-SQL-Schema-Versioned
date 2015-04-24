cde9032fb3e9ce3fa64dd66b358424d1aafbaf0e
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-24 14:18:46 +0700

    Test database schemas are equal between one created directly with latest version's SQL vs upgraded from v1

--- lib/Test/SQL/Schema/Versioned.pm
+++ lib/Test/SQL/Schema/Versioned.pm
@@ -13,6 +13,7 @@ our @EXPORT = qw(
                     sql_schema_spec_ok
             );
 
+use DBIx::Diff::Schema qw(diff_db_schema);
 use SQL::Schema::Versioned qw(create_or_update_db_schema);
 use Test::Exception;
 use Test::More;
@@ -29,28 +30,34 @@ sub sql_schema_spec_ok {
             ok($spec->{install_v1}, 'has install_v1') or return;
         }
 
+        my $dbh1;
         subtest "testing schema creation using install" => sub {
-            my $dbh = $twdb->create_db;
+            $dbh1 = $twdb->create_db;
             lives_ok {
-                my $res = create_or_update_db_schema(dbh=>$dbh, spec=>$spec);
+                my $res = create_or_update_db_schema(dbh=>$dbh1, spec=>$spec);
                 is($res->[0], 200, 'create/update status') or diag explain $res;
             };
         };
 
+        my $dbh2;
         subtest "testing schema upgrade from v1" => sub {
-            my $dbh = $twdb->create_db;
+            $dbh2 = $twdb->create_db;
             lives_ok {
                 local $spec->{install} = $spec->{install_v1};
                 local $spec->{latest_v} = 1;
-                my $res = create_or_update_db_schema(dbh=>$dbh, spec=>$spec);
+                my $res = create_or_update_db_schema(dbh=>$dbh2, spec=>$spec);
                 is($res->[0], 200, 'create/update status') or diag explain $res;
             } "create with install_v1";
 
             lives_ok {
-                my $res = create_or_update_db_schema(dbh=>$dbh, spec=>$spec);
+                my $res = create_or_update_db_schema(dbh=>$dbh2, spec=>$spec);
                 is($res->[0], 200, 'create/update status') or diag explain $res;
             };
         } if $spec->{latest_v} > 1;
+
+        my $diff = diff_db_schema($dbh1, $dbh2);
+        is_deeply($diff, {}, 'create from scratch vs upgrade from v1 results in the same database schema')
+            or diag explain $diff;
     };
 }
 
